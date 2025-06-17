2c9a8ed0e841dc7bd6c42e5309eea45ea0e9062f
Kboy2000 <ebukaakumuo@gmail.com>
2025-06-17 15:59:36 +0100

    added user routes

---
+++ routes/userRoute.js
@@ -0,0 +1,12 @@
+ const express = require('express'); // Imports Express
+ const router = express.Router(); // Creates router
+ const { registerUser, loginUser, getMe } = require('../controllers/userController'); // Imports controllers
+ const { protect } = require('../middleware/authMiddleware'); // Imports middleware 
+
+ 
+router.post('/register', registerUser); // Registration route 
+router.post('/login', loginUser); // Login route 
+router.get('/me', protect, getMe); // Protected user data route 
+
+
+module.exports = router; // Exports router
