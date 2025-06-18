552557641bb3984e76624aca5bdf6c514e1d27df
Kboy2000 <ebukaakumuo@gmail.com>
2025-06-18 15:24:42 +0100

    added a home page

--- views/Home.jsx
+++ views/Home.jsx
@@ -1,17 +1,17 @@
-// import React from "react"; // Imports React
+import React from "react"; // Imports React
 
-// const Home = () => {
-//   // Defines Home component
-//   return (
-//     // Returns JSX
-//     <div className="container">
-//       <h1>Welcome to the Auth App</h1> {/* Page title */}
-//       <p>
-//         Navigate to <a href="/login">Login</a> or <a href="/signup">Signup</a>.
-//       </p>{" "}
-//       {/* Navigation links */}
-//     </div>
-//   );
-// };
+const Home = () => {
+  // Defines Home component
+  return (
+    // Returns JSX
+    <div className="container">
+      <h1>Welcome to the Auth App</h1> {/* Page title */}
+      <p>
+        Navigate to <a href="/login">Login</a> or <a href="/signup">Signup</a>.
+      </p>{" "}
+      {/* Navigation links */}
+    </div>
+  );
+};
 
-// export default Home; // Exports component
+export default Home; // Exports component
