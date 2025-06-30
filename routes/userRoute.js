fccad9fb5bd5b62dcc4bb5b581bc3f1f5d44aecb
Kboy2000 <132431722+Kboy2000@users.noreply.github.com>
2025-06-30 15:31:28 +0100

    Update userRoute.js

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,11 +1,11 @@
  const express = require('express'); // Imports Express
  const router = express.Router(); // Creates router
  const { registerUser, loginUser, getMe } = require('../controllers/userController'); // Imports controllers
- const { protect } = require('../middleware/authMiddleware'); // Imports middleware 
+ const { protect } = require('../middleware/authMiddleware'); // Imports middleware from authMiddleware
 
  
 router.post('/register', registerUser); // Registration route 
-router.post('/login', loginUser); // Login route 
+router.post('/login', loginUser); // Login route
 router.get('/me', protect, getMe); // Protected user data route 
 
 
